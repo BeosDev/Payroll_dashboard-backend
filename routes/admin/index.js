3a67cd12b92bd115543a6a1dc604d174872601de
beosdev <blhl1210@gmail.com>
2018-04-21 00:08:33 +0700

    edit admin

--- routes/admin/index.js
+++ routes/admin/index.js
@@ -3,13 +3,11 @@ var auth = require('../auth');
 
 router.use(auth.authAdmin);
 
-router.use('/alert-aniversary',require('./alert_anniversary.admin'));
-router.use('/alert-benefit-plan',require('./alert_benefit_plan.admin'));
-router.use('/alert-day-offs',require('./alert_day_offs.admin'));
-router.use('/alert-employee-birthday',require('./alert_employee_birthday.admin'));
-router.use('/average-benefit-paid',require('./average_benefit_paid.admin'));
-router.use('/total-earning',require('./total_earning.admin'));
+router.use('./benefit-plans',require('./benefit_plans.admin'));
+router.use('./employment',require('./employment.admin'));
+router.use('/job-history',require('./job_history.admin'));
 router.use('/user-management',require('./user_management.admin'));
-router.use('/vacation-day',require('./vacation_day.admin'));
+router.use('./pay-rates',require('./pay_rates.admin'));
+router.use('./personal',require('./personal.admin'));
 
 module.exports = router;

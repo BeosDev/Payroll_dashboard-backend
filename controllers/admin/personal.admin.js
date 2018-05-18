b5d81bbff20c67ab58d46ab503be81692ee2e701
magicpro97 <linhnt99x@gmail.com>
2018-05-18 22:33:36 +0700

    update

--- controllers/admin/personal.admin.js
+++ controllers/admin/personal.admin.js
@@ -24,28 +24,34 @@ function getPersonPage(req,res) {
 }
 
 function addPerson(req,res) {
+    var gender = '0', shareholderStatus = '0';
+    if(req.body.Add_Gender == 'on')
+        gender = '1';
+    if(req.body.Add_Shareholder_Status == 'on')
+        shareholderStatus = '1';
     var addData = {
-        Employee_ID: res.body.Add_Employee_ID,
-        First_Name: res.body.Add_First_Name,
-        Last_Name:res.body.Add_Last_Name,
-        Middle_Initial:res.body.Add_Middle_Initial,
-        Address1:res.body.Add_Address1,
-        Address2:res.body.Add_Address2,
-        City:res.body.Add_City,
-        State:res.body.Add_State,
-        Zip:res.body.Add_Zip,
-        Email:res.body.Add_Email,
-        Phone_Number:res.body.Add_Phone_Number,
-        Social_Security_Number:res.body.Add_Social_Security_Number,
-        Drivers_License:res.body.Add_Drivers_License,
-        Marital_Status:res.body.Add_Marital_Status,
-        Gender:res.body.Add_Gender,
-        Shareholder_Status:res.body.Add_Shareholder_Status,
-        Benefit_Plans:res.body.Add_Benefit_Plans,
-        Ethnicity:res.body.Add_Ethnicity,
+        Employee_ID: parseInt(req.body.Add_Employee_ID),
+        First_Name: req.body.Add_First_Name,
+        Last_Name:req.body.Add_Last_Name,
+        Middle_Initial:req.body.Add_Middle_Initial,
+        Address1:req.body.Add_Address1,
+        Address2:req.body.Add_Address2,
+        City:req.body.Add_City,
+        State:req.body.Add_State,
+        Zip:req.body.Add_Zip!=''?parseInt(req.body.Add_Zip):0,
+        Email:req.body.Add_Email,
+        Phone_Number:req.body.Add_Phone_Number,
+        Social_Security_Number:req.body.Add_Social_Security_Number,
+        Drivers_License:req.body.Add_Drivers_License,
+        Marital_Status:req.body.Add_Marital_Status,
+        Gender:gender,
+        Shareholder_Status:shareholderStatus,
+        Benefit_Plans: parseInt(req.body.Add_Benefit_Plans),
+        Ethnicity:req.body.Add_Ethnicity,
     }
     var addResult = personModel.addPerson(addData);
     addResult.once('results',function(results){
+        console.log(results);
         res.redirect('/admin/personal');
     });
     addResult.once('error',function(error){
